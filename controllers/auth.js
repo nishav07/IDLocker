0806af5ac05bac9e9ea4ca37109c868c2fc1761f
unknown <nishavkumar07@gmail.com>
2025-12-30 20:09:29 +0530

    working:pass hashing

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const { json } = require('express');
+const middleware = require("../middlewares/auth.js");
 const User = require("../models/user.js");
 const docs = require("../models/document.js");
 
@@ -6,14 +7,22 @@ function auth (req,res){
     res.render("auth.ejs")
 }
 
-function signup (req,res){
-    const {userName,password,email} = req.body;
+async function signup (req,res){
+   try {
+     const {userName,password,email} = req.body;
     console.log("signup wala data:",{
         userName,
         password,
         email
     })
+    const hashPass = await middlewares.hashing(password);
+    
+    const user = new User({ userName:userName,password:hashPass,email:email});
+    await user.save();
     res.send('ok hai boss')
+   } catch (error) {
+    console.log(error);
+   }
 }
 
 function login (req,res){
