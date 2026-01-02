5e9a1a37a27978333ead2ee6ea1990f6b630776f
unknown <nishavkumar07@gmail.com>
2026-01-02 20:01:19 +0530

    start:degining layouts

--- public/js/layout.js
+++ public/js/layout.js
@@ -2,12 +2,11 @@
   const flash = document.getElementById("flash");
 
   if (flash) {
-    // show
     setTimeout(() => {
       flash.classList.remove("opacity-0", "translate-y-6");
     }, 50);
 
-    // hide
+
     setTimeout(() => {
       flash.classList.add("opacity-0", "translate-y-6");
     }, 3000);
