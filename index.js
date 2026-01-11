9fb25499b3b283f1217cd09d5b0ab9d271ba222f
unknown <nishavkumar07@gmail.com>
2026-01-11 18:50:58 +0530

    completed

--- index.js
+++ index.js
@@ -7,16 +7,15 @@ const bcrypt = require("bcrypt");
 const flash = require("connect-flash");
 const ejs = require("ejs");
 const mongooese = require('mongoose');
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname,"public")));
 app.use(express.urlencoded({ extended: true }));
 
-// app.set("public");
+
 app.set("views",path.join(__dirname,"views"));
 app.set("view engine","ejs");
-// app.use(express.static(path.join(__dirname , "public")));
 
 const sessionConfig = require("./config/session");
 const db = require("./config/db.js");
